78031f9ed7a295fd87cdac7233479b080cc074ab
Ashuiiit <ashutosh20287@iiitd.ac.in>
2021-10-11 23:41:13 +0530

    added

---
+++ src/hospital.java
@@ -0,0 +1,4 @@
+public class hospital {
+    int student;
+    int boys;
+}
---
+++ src/vaccine.java
@@ -0,0 +1,12 @@
+ class vaccine {
+    String name;
+    int doseNumberReq;
+    int gap;
+
+    vaccine(String name, int doseNumberReq, int gap){
+        this.name=name;
+        this.doseNumberReq=doseNumberReq;
+        this.gap=gap;
+        System.out.println("Vaccine Name: "+this.name);
+    }
+}
